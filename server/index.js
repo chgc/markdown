0f9ce050f89a778b96d87bc4c008b8b9239b135a
Kevin Yang <chgc_kevin@hotmail.com>
2016-05-02 17:47:00 +0800

    refactor createAlbum and addphoto function

--- server/index.js
+++ server/index.js
@@ -28,16 +28,74 @@ app.get("/", function (req, res) {
 });
 
 app.post('/upload', function (req, res) {
-    //生成multiparty對象，並配置上傳目標路徑
+    /**
+         * @description 檢查相簿是否存在，如果不存在則建立該相簿
+         * @param  {any} flickr
+         * @param  {any} photo_id
+         * @param  {any} blogname
+         */
+    function createAlbum(arg) {
+        var flickr = arg.flickr,
+            blogname = arg.blogname,
+            photo_id = arg.photo_id;
+        if (!blogname) blogname = "blogphotos";
+        return new Promise(function (resolve, reject) {
+            flickr.photosets.getList({
+                api_key: flickr.options.api_key,
+                user_id: flickr.options.user_id
+            }, function (err, result) {
+                if (!err) {
+                    // check if album "blogphotos" exist
+                    var album = result.photosets.photoset.filter(function (album) {
+                        return album.title._content == blogname;
+                    });
+                    if (album.length == 0) {
+                        flickr.photosets.create({
+                            api_key: flickr.options.api_key,
+                            title: blogname,
+                            primary_photo_id: photo_id
+                        }, function (err, result) {
+                            resolve(result.photoset)
+                        });
+                    } else {
+                        resolve({ flickr: flickr, photo_id: photo_id, album: album[0] })
+                    }
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    }
+    /**
+     * @description 新增照片到照片集中
+     * @param  {any} flickr
+     * @param  {any} photo_id
+     * @param  {any} album
+     */
+    function addPhoto(arg) {
+        var flickr = arg.flickr,
+            photo_id = arg.photo_id,
+            album = arg.album;
+        return new Promise(function (resolve, reject) {
+            flickr.photosets.addPhoto({
+                api_key: flickr.options.api_key,
+                photoset_id: album.id,
+                photo_id: photo_id
+            }, function (err, result) {
+                if(err) reject(err);
+                resolve(arg)
+            })
+        });
+    }
+    //生成multiparty對象
     var form = new multiparty.Form();
-
     //上傳完成後處理
     form.parse(req, function (err, fields, files) {
-
         if (err) {
             console.log('parse error: ' + err);
         } else {
             var inputFile = files.file[0];
+
 
             Flickr.authenticate(FlickrOptions, function (error, flickr) {
                 var uploadOptions = {
@@ -51,50 +109,16 @@ app.post('/upload', function (req, res) {
                         console.error(error);
                     } else {
                         var photo_id = result;
-                        var photoset_id = -1;
-                        flickr.photosets.getList({
-                            api_key: flickr.options.api_key,
-                            user_id: flickr.options.user_id
-                        }, function (err, result) {                            
-                            if (!err) {
-                                // check if album "blogphotos" exist
-                                var photosets = result.photosets.photoset;
-                                var album = photosets.filter(function (album) {
-                                    return album.title._content == "blogphotos";
-                                });
-                                if (album.length == 0) {
-                                    flickr.photosets.create({
-                                        api_key: flickr.options.api_key,
-                                        title: "blogphotos",
-                                        description: "for blog image usage",
-                                        primary_photo_id: photo_id
-                                    }, function (err, result) {
-                                        photoset_id = result.photoset.id;
-                                        flickr.photosets.addPhoto({
-                                            api_key: flickr.options.api_key,
-                                            photoset_id: photoset_id,
-                                            photo_id: photo_id
-                                        }, function (err, result) {
-
-                                        })
-                                    });
-                                } else {                                    
-                                    photoset_id = album[0].id;
-                                    flickr.photosets.addPhoto({
-                                        api_key: flickr.options.api_key,
-                                        photoset_id: photoset_id,
-                                        photo_id: photo_id
-                                    }, function (err, result) {
-
-                                    })
-                                }
-                            }
-                        });
-
+                        // check/create album and add photo into album
+                        createAlbum({ flickr: flickr, photo_id: photo_id })
+                            .then(addPhoto)
+                            .catch(function (error) {
+                                console.error(error);
+                            });
 
                         flickr.photos.getSizes({
                             api_key: flickr.options.api_key,
-                            photo_id: result
+                            photo_id: photo_id
                         }, function (err, result) {
                             if (result) {
                                 var obj = result.sizes.size.filter(function (x) {
@@ -107,8 +131,6 @@ app.post('/upload', function (req, res) {
                         });
                     }
                 });
-
-
             });
 
         }
